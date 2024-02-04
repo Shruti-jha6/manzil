f952e294518dcdbecde633bb2b1a5102a6414275
Shruti-jha6 <shruti119btcsai22@igdtuw.ac.in>
2024-02-04 14:58:42 +0530

    sixth

---
+++ manzill/src/comp/Grow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Grow = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Grow
---
+++ manzill/src/comp/Legal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Legal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Legal
